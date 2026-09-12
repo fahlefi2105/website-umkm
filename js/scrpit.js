4aa6612d18a2dbb4ef8577b53cc29fba6107b9a6
unknown <fahlefifirza47@gmail.com>
2026-09-12 16:58:05 +0700

    feat: tambah informasi lokasi UMKM

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,6 +1,8 @@
 const promoButton = document.querySelector('.promo-button');
+const promoText = document.querySelector('.promo-text');
 
 promoButton.addEventListener('click', () => {
-    promoButton.textContent = "Promo beli 2 gratis  1";
-    console.log("Promo Kopi nusa berhasil ditampilkan.");
+    promoText.style.display = 'block';
+    promoButton.textContent = "Beli 2 Gratis 1, khusus hari ini!";
+
 });
