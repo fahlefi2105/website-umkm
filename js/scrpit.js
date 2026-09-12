574ac2cd336e19b70f1555b4566b3e15838d4a73
unknown <fahlefifirza47@gmail.com>
2026-09-12 19:41:08 +0700

    feat: tambah informasi lokasi,jam, produk dan promoUMKM

--- js/scrpit.js
+++ js/scrpit.js
@@ -4,4 +4,5 @@ const promoText = document.querySelector('.promo-text');
 promoButton.addEventListener('click', () => {
     promoText.style.display = 'block';
     promoButton.textContent = "Beli 2 Gratis 1, khusus hari ini!";
+    console.log("Terima kasih sudah mampir ke Kopi Nusa!");
 });
