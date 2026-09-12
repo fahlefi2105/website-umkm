629097f199a7eeaf0f44e6bdc17289aeb9443660
unknown <fahlefifirza47@gmail.com>
2026-09-12 19:07:34 +0700

    feat: tambah informasi lokasi,jam, produk dan promoUMKM

--- js/scrpit.js
+++ js/scrpit.js
@@ -4,5 +4,4 @@ const promoText = document.querySelector('.promo-text');
 promoButton.addEventListener('click', () => {
     promoText.style.display = 'block';
     promoButton.textContent = "Beli 2 Gratis 1, khusus hari ini!";
-
 });
