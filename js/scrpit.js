48f224c3a5f467d3f46ee1bb2617187598d02ae2
unknown <fahlefifirza47@gmail.com>
2026-09-16 22:37:30 +0700

    menambahkan semantik html dan halaman navigasi

--- js/scrpit.js
+++ js/scrpit.js
@@ -3,6 +3,6 @@ const promoText = document.querySelector('.promo-text');
 
 promoButton.addEventListener('click', () => {
     promoText.style.display = 'block';
-    promoButton.textContent = "Beli 2 Gratis 1, khusus hari ini!";
-    console.log("Terima kasih sudah mampir ke Kopi Nusa!");
+    promoButton.textContent = 'Beli 2 Gratis 1, khusus hari ini!';
+    console.log('Terima kasih sudah mampir ke Kopi Nusa!');
 });
